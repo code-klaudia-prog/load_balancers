14688624d93092bec93fafd310a246fde0047525
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2025-09-19 11:23:16 +0100

    Implemented Absolute Path

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,6 +1,6 @@
 # --- Módulo de Rede ---
 module "network" {
-  source               = "./modules/network"
+  source               = "/modules/network"
   resource_group_name  = "example-resources"
   location             = "West Europe"
   vnet_name            = "meu-vnet-producao"
