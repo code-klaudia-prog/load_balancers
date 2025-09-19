d6c81acc8705e2685def9309af2f4af011e31e9c
cssilva99 <46228708+cssilva99@users.noreply.github.com>
2025-09-19 11:51:58 +0100

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,6 +1,6 @@
 # --- Databse Module ---
 module "database" {
-  source               = "company_x/database/azurerm"
+  source               = "git::https://github.com/seu-utilizador/repositorio-de-modulos.git//app_services"
   resource_group_name  = "example-resources"
   location             = "West Europe"
   vnet_id              = module.network.vnet_id
